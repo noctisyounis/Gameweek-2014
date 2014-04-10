018314f06f7a716ba296bca4a18ae85c4cddacee
noctisyounis <cherifyounis@gmail.com>
2014-04-10 18:04:42 +0200

    Polygone function and changed GameObject

--- Assets/Scripts/Javascript/Tools/CanvasExtension.js
+++ Assets/Scripts/Javascript/Tools/CanvasExtension.js
@@ -16,3 +16,22 @@ function(x,y,w,h,r) {
     this.closePath();
     this.fill();
 };
+
+CanvasRenderingContext2D.prototype.fillPolygon = function (pointsArray, fillColor,     strokeColor) {
+    if (pointsArray.length <= 0) return;
+    this.moveTo(pointsArray[0][0], pointsArray[0][1]);
+    for (var i = 0; i < pointsArray.length; i++) {
+        this.lineTo(pointsArray[i][0], pointsArray[i][1]);
+    }
+    if (strokeColor != null && strokeColor != undefined)
+        this.strokeStyle = strokeColor;
+
+    if (fillColor != null && fillColor != undefined) {
+        this.fillStyle = fillColor;
+        this.fill();
+    }
+}
+/*
+var polygonPoints = [[10,100],[20,75],[50,100],[100,100],[10,100]];
+context.fillPolygon(polygonPoints, '#F00','#000');
+*/
