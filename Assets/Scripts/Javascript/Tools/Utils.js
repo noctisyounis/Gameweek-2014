83371ccc7c0fcb54c333534bff9045e2611afe6a
noctisyounis <cherifyounis@gmail.com>
2014-04-10 13:51:33 +0200

    Elastic, Back & Bounce added + archi changed

--- Assets/Scripts/Javascript/Tools/Utils.js
+++ Assets/Scripts/Javascript/Tools/Utils.js
@@ -20,14 +20,6 @@ Tween =
 {
 	/* Name of functions:
 
-	* Quad 	= Quadratic
-	* Cubic = Cubic
-	* Quart = Quartic
-	* Qunt 	= Quintic
-	* Sine 	= Sinusoidale
-	* Expo 	= Exponential
-	* Circ 	= Circle
-
 	* Linear: 	 	 no easing no accelleration 
 	* In:  			 accelerating from zero velocity 
 	* Out: 			 decelerating to zero velocity
@@ -46,34 +38,78 @@ Tween =
 
 
 	Linear: function (t, b, c, d) {return c*t/d + b;},
-	// Quadratic
-	InQuad: function (t, b, c, d) {t /= d;return c*t*t + b;}, 
-	OutQuad: function (t, b, c, d) {t /= d;return -c * t*(t-2) + b;},
-	InOutQuad: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t + b;t--;return -c/2 * (t*(t-2) - 1) + b;},
-	// Cubic
-	InCubic: function (t, b, c, d) {t /= d;return c*t*t*t + b;},
-	OutCubic: function (t, b, c, d) {t /= d;t--;return c*(t*t*t + 1) + b;},
-	InOutCubic: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t + b;t -= 2;return c/2*(t*t*t + 2) + b;},
-	// Quartic
-	InQuart: function (t, b, c, d) {t /= d;return c*t*t*t*t + b;},
-	OutQuart: function (t, b, c, d) {t /= d;t--;return -c * (t*t*t*t - 1) + b;},
-	InOutQuart: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t*t + b;t -= 2;return -c/2 * (t*t*t*t - 2) + b;},
-	// Quintic
-	InQuint: function (t, b, c, d) {t /= d;return c*t*t*t*t*t + b;},
-	OutQuint: function (t, b, c, d) {t /= d;t--;return c*(t*t*t*t*t + 1) + b;},
-	InOutQuint: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t*t*t + b;t -= 2;return c/2*(t*t*t*t*t + 2) + b;},
-	// Sinusoidal
-	InSine: function (t, b, c, d) {return -c * Math.cos(t/d * (Math.PI/2)) + c + b;},
-	OutSine: function (t, b, c, d) {return c * Math.sin(t/d * (Math.PI/2)) + b;},
-	InOutSine: function (t, b, c, d) {return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;},
-	// Exponential
-	InExpo: function (t, b, c, d) {return c * Math.pow( 2, 10 * (t/d - 1) ) + b;},
-	OutExpo: function (t, b, c, d) {return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;},
-	InOutExpo: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;t--;return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;},
-	// Circlear
-	InCirc: function (t, b, c, d) {t /= d;return -c * (Math.sqrt(1 - t*t) - 1) + b;},
-	OutCirc: function (t, b, c, d) {t /= d;t--;return c * Math.sqrt(1 - t*t) + b;},
-	InOutCirc: function (t, b, c, d) {t /= d/2;if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;t -= 2;return c/2 * (Math.sqrt(1 - t*t) + 1) + b;}
+
+	Quadratic: 
+	{
+		In: function (t, b, c, d) {t /= d;return c*t*t + b;}, 
+		Out: function (t, b, c, d) {t /= d;return -c * t*(t-2) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t + b;t--;return -c/2 * (t*(t-2) - 1) + b;}
+	},
+
+	Cubic:
+	{
+		In: function (t, b, c, d) {t /= d;return c*t*t*t + b;},
+		Out: function (t, b, c, d) {t /= d;t--;return c*(t*t*t + 1) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t + b;t -= 2;return c/2*(t*t*t + 2) + b;},
+	},
+
+	Quartic:
+	{
+		In: function (t, b, c, d) {t /= d;return c*t*t*t*t + b;},
+		Out: function (t, b, c, d) {t /= d;t--;return -c * (t*t*t*t - 1) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t*t + b;t -= 2;return -c/2 * (t*t*t*t - 2) + b;},
+	},
+	
+	Quintic:
+	{
+		In: function (t, b, c, d) {t /= d;return c*t*t*t*t*t + b;},
+		Out: function (t, b, c, d) {t /= d;t--;return c*(t*t*t*t*t + 1) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2*t*t*t*t*t + b;t -= 2;return c/2*(t*t*t*t*t + 2) + b;},
+	},
+
+	Sinusoidale:
+	{
+		In: function (t, b, c, d) {return -c * Math.cos(t/d * (Math.PI/2)) + c + b;},
+		Out: function (t, b, c, d) {return c * Math.sin(t/d * (Math.PI/2)) + b;},
+		InOut: function (t, b, c, d) {return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;},	
+	},
+
+
+	Exponential:
+	{
+		In: function (t, b, c, d) {return c * Math.pow( 2, 10 * (t/d - 1) ) + b;},
+		Out: function (t, b, c, d) {return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;t--;return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;}
+	},
+	
+
+	Circular:
+	{
+		In: function (t, b, c, d) {t /= d;return -c * (Math.sqrt(1 - t*t) - 1) + b;},
+		Out: function (t, b, c, d) {t /= d;t--;return c * Math.sqrt(1 - t*t) + b;},
+		InOut: function (t, b, c, d) {t /= d/2;if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;t -= 2;return c/2 * (Math.sqrt(1 - t*t) + 1) + b;}
+	},
+
+	Elastic: 
+	{
+		In: function (t, b, c, d, a, p) {if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3; if (a < Math.abs(c)) { a=c; var s=p/4; }else var s = p/(2*Math.PI) * Math.asin (c/a);return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;},
+		Out: function (t, b, c, d, a, p) {if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;if (a < Math.abs(c)) { a=c; var s=p/4; } else var s = p/(2*Math.PI) * Math.asin (c/a);return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;},
+		InOut:function (t, b, c, d, a, p) {if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);if (a < Math.abs(c)) { a=c; var s=p/4; }else var s = p/(2*Math.PI) * Math.asin (c/a);if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;}	
+	},
+
+	Back: 
+	{
+		In: function (t, b, c, d, s) {if (s == undefined) s = 1.70158;return c*(t/=d)*t*((s+1)*t - s) + b;},
+		Out: function (t, b, c, d, s) {if (s == undefined) s = 1.70158;return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;},
+		InOut: function (t, b, c, d, s) {if (s == undefined) s = 1.70158; if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;}
+	},	
+
+	Bounce:
+	{
+		In: function (t, b, c, d) {return c - Tween.Bounce.Out (d-t, 0, c, d) + b;},
+		Out: function (t, b, c, d) {if ((t/=d) < (1/2.75)) {return c*(7.5625*t*t) + b;} else if (t < (2/2.75)) {return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;} else if (t < (2.5/2.75)) {return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;} else {return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;}},
+		InOut: function (t, b, c, d) {if (t < d/2) return Tween.Bounce.In (t*2, 0, c, d) * .5 + b;return Tween.Bounce.Out (t*2-d, 0, c, d) * .5 + c*.5 + b;}
+	}
 };
 
 Time = 
@@ -95,14 +131,7 @@ Time =
 	TimeOfLastFrame: 0,
 
 	LevelLoaded: function() { Time.TimeWhenLevelLoaded = new Date().getTime();},
-	SetTimeValues: function ()
-	{
-		this.Time = Date.now(); 
-		this.DeltaTime = (this.Time - this.TimeOfLastFrame) / 1000; 
-		this.avgDelay += ((this.Time - this.TimeOfLastFrame) - this.avgDelay) / 10;
-		this.Fps = (1000/this.avgDelay).toFixed(1);
-		this.TimeOfLastFrame = this.Time;
-	}
+	SetTimeValues: function (){this.Time = Date.now(); this.DeltaTime = (this.Time - this.TimeOfLastFrame) / 1000; this.avgDelay += ((this.Time - this.TimeOfLastFrame) - this.avgDelay) / 10;this.Fps = (1000/this.avgDelay).toFixed(1);this.TimeOfLastFrame = this.Time;}
 };
 
 Dialogue = 
