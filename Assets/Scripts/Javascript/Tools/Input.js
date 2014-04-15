ade012d89c9d21f0ebb474164aa83c8028e8f9c9
noctisyounis <cherifyounis@gmail.com>
2014-04-15 13:59:34 +0200

    Mouse Improvement, battle system added

--- Assets/Scripts/Javascript/Tools/Input.js
+++ Assets/Scripts/Javascript/Tools/Input.js
@@ -1,19 +1,29 @@
 function mouseDown(event)
-{
-	Input.MouseClick = true;
+{	
+	if(!Input.MouseClick && Input.MouseReload)
+	{
+		Input.MouseClick = true;
+		Input.MouseReload = false;
+	}
+	else if(!Input.MouseReload)
+	{
+		Input.MouseClick = false;
+	}
+	Input.MouseLongClick = true;
 	mouseMove(event);
 }
 function mouseUp(event)
 {
 	Input.MouseClick = false;
+	Input.MouseLongClick = false;
+	Input.MouseReload = true;
 	Input.MouseDraging = false;
 	Input.DraggedElement = null;
 	mouseMove(event);
 }
 function mouseMove(event)
 {
-	var rect = canvas.getBoundingClientRect();
-
+	var rect = canvas.getBoundingClientRect();	
 	Input.MousePosition.x = (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
 	Input.MousePosition.y = (event.clientY - canvas.offsetTop) / (rect.bottom - rect.top) * canvas.height;
 }
