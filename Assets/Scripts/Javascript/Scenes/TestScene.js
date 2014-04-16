0cd45fe73f5710cbe413d9540d1294fc094b836f
Molokax <molokax@hotmail.fr>
2014-04-16 11:34:15 +0200

    nope

---
+++ Assets/Scripts/Javascript/Scenes/TestScene.js
@@ -0,0 +1,108 @@
+/*	**** For create a new Scene **** 
+*
+*	@step 1							Copy the content of this file in a new .js document.
+*   ----------------------------------------------------------------------------------------------------------------------------
+*	@step 2							Save the new file in Assets/Javascript/Scenes/NameOfYourScene.js .
+*   ----------------------------------------------------------------------------------------------------------------------------
+*	@step 3                      	In the index.html add below this comment <!-- Scene --> the line: 
+*                    "<script type="text/javascript" src="Assets/Scripts/Javascript/Scenes/NameOfYourGameObject.js"></script>"
+*	----------------------------------------------------------------------------------------------------------------------------
+*	@step 4						    For create a new scene, use this instruction: "new Scene()".
+*/
+
+/*	**** How to make the setup of a Scene ****
+*	
+*	@property name 																											{string} 			 
+*	The name of the scene.
+*	--------------------------------------------------------------------------------------------------------------------------------
+*	@property GameObjects 																				   {array[GameObject1, ...]} 			 
+*	All the GameObject of the scene	
+*
+*/
+
+/*	**** Method of the scene ****
+*
+*	@method Awake()									
+*	Called at the instruction new Scene().
+*	--------------------------------------------------------------------------------------------------------------------------------
+*	@method Start()									
+*	Called at the first use of scene in game.
+*	--------------------------------------------------------------------------------------------------------------------------------
+*	@method Update()								
+*	Called each frame,code game is here.
+*	--------------------------------------------------------------------------------------------------------------------------------
+*	@method LateUpdate()
+*	Called each frame, code all the GUI here.
+*/
+
+/* **** For launch Scene ****
+*
+*	To load your scene, use this instruction: "Application.LoadLevel(LevelName)".
+*/
+
+
+
+function SceneTest () 
+{
+	this.name = "Test";
+	this.Started = false;
+
+	this.GameObjects = [];
+
+	this.Awake = function()
+	{
+		//codez l'awake avant le console.log
+		console.clear();
+		console.log(" %c System: Scene " + this.name + " created!", 'background: #222; color: #bada55'); 
+	};
+
+	this.Start = function()
+	{
+		if(!this.Started)
+		{
+			//codez le start avant le changement de booleen(Images)
+			this.GameObjects.push(new GameObjectTest());
+
+			this.Started = true;
+			Time.LevelLoaded();
+			console.log(" %c System: Scene " + this.name + " have started!", 'background: #222; color: #bada55');
+		}
+		this.Update();
+	};
+
+	this.Update = function()
+	{
+		if(!Application.GamePaused)
+		{
+			ctx.fillStyle = "black";
+			ctx.fillRect(0,0, canvas.width, canvas.height);
+
+			//Faire un drawImage du background de la scene en question
+			ctx.drawImage(Images.ceilingBackground, 0, 0, canvas.width, canvas.height);
+			for(var i = 0; i < this.GameObjects.length; i++)
+			{
+				if(this.GameObjects[i].enabled)
+				{
+					this.GameObjects[i].Start();
+				}
+			}	
+			if(!Dialogue.finished) {Dialogue.Continue();}
+			this.LateUpdate();
+		}
+	};
+
+	this.LateUpdate = function()
+	{
+		if(!Application.GamePaused)
+		{
+			//Codez la GUI ici pour que la pause soit prise en compte
+		}
+		if(Application.DebugMode)
+		{
+			Debug.ShowStats();
+		}
+	};
+
+	// lance l'awake a la creation de la scene
+	this.Awake();
+}
